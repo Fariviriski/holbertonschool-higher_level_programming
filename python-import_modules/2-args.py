cb1755ee47b797c31e4714a7f1f3dbff64576e59
Fariviriski <mafu.is.here@gmail.com>
2024-01-26 00:45:15 -0400

    what are these checkers

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,10 +7,10 @@ if c == 0:
     print("0 arguments.")
 
 elif c == 1:
-    print("1 argument.")
+    print("1 argument:")
 
 else:
-    print("{} arguments.".format(c))
+    print("{} arguments:".format(c))
 
 for i in range(c):
     print("{}: {}".format(i + 1, sys.argv[i + 1]))
