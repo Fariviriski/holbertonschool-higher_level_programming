54d3ef1032c5c84288447ed570f7947e1fcc370f
Fariviriski <mafu.is.here@gmail.com>
2024-02-02 13:15:21 -0400

    changed tests

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,37 +1,45 @@
 #!/usr/bin/python3
-"""
-unittest for max_integer([...])
-"""
+"""Unittest for max_integer([..])"""
+
+
 import unittest
-max_integer = __import__('6-max_integer').max_integer
 
-class TextMaxInteger(unittest.TestCase):
-    """max_integer tests"""
-    def setup(self):
-        self.ordred_list = [1, 2, 3, 4]
-        slef.unordered_list = [4, 2, 1, 3]
+max_integer = __import__("6-max_integer").max_integer
 
-    def tesr_mxs(self):
-        delf.assertEqual(max_integer(self.ordred_list),
-                         max_integer(self.unordered_list))
 
-    def test_str(self):
-        self.assertEqual(max_integer("string"), 't')
+class TestMaxInteger(unittest.TestCase):
+    """Unittests for the function max_integer"""
 
-    def test_none(self):
-        self.assertEqual(max_integer(), None)
+    def test_empty(self):
+        """Test an empty list"""
+        empty = []
+        self.assertEqual(max_integer(empty), None)
 
-    def test_list_negatives(self):
-        with self.assertEqual(max_integer([-420, -69, -97]), -1)
+    def test_one(self):
+        """Test a list with one integer"""
+        one_element = [1]
+        self.assertEqual(max_integer(one_element), 1)
 
-    def test_ky_err(self):
-        with self.assertRaises(KeyError):
-            max_integer({5 : 6})
+    def test_sorted(self):
+        """Test a sorted list of integers"""
+        ordered = [32, 64, 128, 256]
+        self.assertEqual(max_integer(ordered), 256)
 
-    def test_2many_args(self):
-        with self.assertRaises(TypeError):
-            max_integer('tuple', 'strings')
+    def test_sorted_rev(self):
+        """Test a sorted (in reverse) list of integers"""
+        ordered = [1024, 512, 256, 128, 64]
+        self.assertEqual(max_integer(ordered), 1024)
 
-    def test_incompatible_items(self):
-        with self.assertRaises(TypeError):
-            max_integer([[], {}])
+    def test_random(self):
+        """Test a list of integers"""
+        ordered = [98, -1024, 1, -21, 2795]
+        self.assertEqual(max_integer(ordered), 2795)
+
+    def test_negative(self):
+        """Test a list of negative integers"""
+        ordered = [-35, -1024, -1, -21]
+        self.assertEqual(max_integer(ordered), -1)
+
+
+if __name__ == "__main__":
+    unittest.mai
