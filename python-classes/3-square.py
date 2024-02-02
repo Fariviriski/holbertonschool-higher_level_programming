98bb098d590b2411903fcfa05d1c3baa00945dae
Fariviriski <mafu.is.here@gmail.com>
2024-02-01 21:55:31 -0400

    unicode tests dont work

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 """Square module."""
 
+
 class Square:
     """initialize instance attributes methods"""
+
     def __init__(self, size=0):
-            TypeError: if size is not an integer
-            ValueError: if size < 0
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("value must be >= 0")
-        self.__size = size
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+            self.size = size
 
-        def area(self):
-            return self.__size ** 2
+    def area(self):
+        return self.size * self.size
