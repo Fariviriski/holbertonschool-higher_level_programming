7ef3d5b722d89798807853e37e1645d79262d2d3
Fariviriski <mafu.is.here@gmail.com>
2024-02-01 21:46:32 -0400

    lmao

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-'''Square'''
+"""Square"""
 
 
 class Square:
-    '''Square'''
+    """attribute"""
+
     def __init__(self, size=0):
         self.size = size
-        '''raise an error if the data type is incorrect'''
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-         '''raise an error if the data is less that 0'''
         if size < 0:
             raise ValueError("size must be >= 0")
