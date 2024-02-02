111b412fddcea208053285245d2ccbd825d8fccd
Fariviriski <mafu.is.here@gmail.com>
2024-02-01 22:04:36 -0400

    aaaa

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -5,20 +5,22 @@
 class Square:
     """Square class define initialize attributes methods"""
 
-    @property
-    def get_size(self):
+     @property
+    def size(self):
         return self.__size
 
-    @get_size.setter
+    @size.setter
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if value < 0:
+
+        elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
+        else:
+            self.__size = value
 
-        def __init__(self, size=0):
-            self.size = size
+    def __init__(self, size=0):
+        self.size = size
 
-        def area(self):
-            return self.__size * self.__size
+    def area(self):
+        return self.size * self.size
