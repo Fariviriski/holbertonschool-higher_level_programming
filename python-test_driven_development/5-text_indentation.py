4939d6970d849b96d8532d27dc4f392487bae0bf
Fariviriski <mafu.is.here@gmail.com>
2024-02-02 12:58:17 -0400

    simpler aproach

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -11,31 +11,10 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    delimiters = [".", "?", ":"]
+    for c in [".", "?", ":"]:
+        if c in text:
+            text = text.replace(c, c + "\n\n\a")
 
-    strt = 0
-
-    for i, char in enumerate(text):
-        if char in delimiters:
-            print(text[strt : i + 1].strip())
-            print()
-            strt = i + 1
-
-    if strt < len(text):
-        print(text[strt:].strip())
-
-
-if __name__ == "__main__":
-    text_indentation(
-        """
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
-    Quonam modo? Utrum igitur tibi litteram videor an totas paginas commovere? \
-    Non autem hoc: igitur ne illud quidem. Fortasse id optimum, sed ubi illud: \
-    Plus semper voluptatis? Teneo, inquit, finem illi videri nihil dolere. \
-    Transfer idem ad modestiam vel temperantiam, quae est moderatio cupiditatum \
-    rationi oboediens. Si id dicis, vicimus. Inde sermone vario sex illa a Dipylo \
-    stadia confecimus. Sin aliud quid voles, postea. Quae animi affectio suum \
-    cuique tribuens atque hanc, quam dico. Utinam quidem dicerent alium alio \
-    beatiorem! Iam ruinas videres
-    """
-    )
+    list_sentence = text.split("\a")
+    for sentence in list_sentence:
+        print(sentence.strip(" "), end="")
