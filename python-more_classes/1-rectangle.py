5b0515b23099465976d1a7b631fa21722426fc19
Fariviriski <mafu.is.here@gmail.com>
2024-02-08 16:19:02 -0400

    defined height and width

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+module that defines Rectangle class
+"""
+
+
+class Rectangle:
+    "defines a rectangle with area"
+
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """The width property."""
+        return self.width
+
+    @property
+    def height(self):
+        """The height property."""
+        return self.height
+
+    @width.setter
+    def width(self, value):
+        """sets the width value of a rectangle"""
+        self.__width = value
+        if isnotinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+    @height.setter
+    def height(self, value):
+        """sets height value of a rectangle"""
+        self._width = value
+        if isnotinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
