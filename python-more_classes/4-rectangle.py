b6a31049230473e1fa4b925dceba0b7d27b83502
Fariviriski <mafu.is.here@gmail.com>
2024-02-08 19:52:01 -0400

    better documentation, fixed some grammar

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -1,67 +1,67 @@
 #!/usr/bin/python3
-"""
-module for rectangle
-"""
+"""class Rectangle"""
 
 
 class Rectangle:
-    """constructor for class named Rectangle"""
+    """Constructor of the empty class Rectangle"""
 
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
 
-        @property
-        def width(self):
-            """The width property."""
-            return self._width
+    @property
+    def width(self):
+        """property for private instance attribute: width"""
+        return self.__width
 
-        @width.setter
-        def width(self, value):
-            if type(value) is not int:
-                raise TypeError("width must be an integer")
-            elif value < 0:
-                raise ValueError("width must be >= 0")
-            else:
-                self._width = value
+    @width.setter
+    def width(self, value):
+        """setter of width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
 
-        @property
-        def height(self):
-            """The height property."""
-            return self._height
+    @property
+    def height(self):
+        """property for private instance attribute: height"""
+        return self.__height
 
-        @height.setter
-        def height(self, value):
-            if type(value) is not int:
-                raise TypeError("height must be an integer")
-            elif value < 0:
-                raise ValueError("height must be >= 0")
-            else:
-                self._height = value
+    @height.setter
+    def height(self, value):
+        """setter of height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
 
-            def area(self):
-                """method to cals area"""
-                return self.width + self.height
+    def area(self):
+        """Method to calculate the area of Rectangle"""
+        return self.width * self.height
 
-            def perimeter(self):
-                """method to calc perimeter"""
+    def perimeter(self):
+        """Method to calculate the perimeter of Rectangle"""
+        if self.width <= 0 or self.height <= 0:
+            return 0
+        return 2 * (self.width + self.height)
 
-            if self.width <= 0 or self.height <= 0:
-                return 0
-            return 2 * (self.width + self.height)
+    def __str__(self):
+        """Print a Rectangle to stdout"""
+        result = ""
+        if self.width <= 0 or self.height <= 0:
+            return result
+        else:
+            for h in range(self.height):
+                for w in range(self.width):
+                    result += "#"
+                if h != (self.height - 1):
+                    result = result + "\n"
+        return result
 
-            def __str__(self):
-                """prints rectangle to stdout"""
-                result = ""
-                if self.width <= 0 or self.height <= 0:
-                    return result
-                else:
-                    for h in range(self.height):
-                        for w in range(self.width):
-                            result += "#"
-                        if h != (self.height - 1):
-                            result = result + "\n"
-                return result
-
-        def __repr__(self):
-            return "Rectangle({:d}, {:d})".format(self.width, self.height)
+    def __repr__(self):
+        """Method that return an Unambigous representation of str"""
+        return "Rectangle({:d}, {:d})".format(self.width, self.height)
