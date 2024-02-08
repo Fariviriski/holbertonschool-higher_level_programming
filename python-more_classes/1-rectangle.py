97eb23555832363a558103cacb96b7f297b59b12
Fariviriski <mafu.is.here@gmail.com>
2024-02-08 16:34:12 -0400

    new order

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -5,38 +5,38 @@ module that defines Rectangle class
 
 
 class Rectangle:
-    "defines a rectangle with area"
+    """Constructor of the empty class Rectangle"""
 
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
-        """The width property."""
+        """property for private instance attribute: width"""
         return self.__width
-
-    @property
-    def height(self):
-        """The height property."""
-        return self.__height
 
     @width.setter
     def width(self, value):
-        """sets the width value of a rectangle"""
-        if isnotinstance(value, int):
+        """setter of width"""
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
-        self.__width = value
-        return self.__width
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """property for private instance attribute: height"""
+        return self.__height
 
     @height.setter
     def height(self, value):
-        """sets height value of a rectangle"""
-        if isnotinstance(value, int):
+        """setter of height"""
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = value
-        return self.__height
+        else:
+            self.__height = value
