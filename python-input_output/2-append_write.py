9639548f2a075587ce01bf5a474cec5f41ca3fc0
Fariviriski <mafu.is.here@gmail.com>
2024-02-09 16:31:02 -0400

    tax

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """append"""
 
+
 def append_write(filename="", text=""):
-   """append
-   Args:
-    filename: name of filename
-    text: text to append
-   """
+    """append
+
+    Args:
+        filename: name of file
+        text: text to append
+    """
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
