b2ec1bc29f27a24a1e9405a0374a8162261f2a5e
Fariviriski <mafu.is.here@gmail.com>
2024-02-09 14:48:14 -0400

    syntax?

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -8,4 +8,5 @@ def is_same_class(obj, a_class):
     """checks if obj is instance of a given class"""
     if type(obj) == a_class:
         return True
-    return False
+    else:
+        return False
