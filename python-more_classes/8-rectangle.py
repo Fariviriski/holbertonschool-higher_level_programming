16a3bbc4c54e18f1d49d5cb23a08d925a4776412
Fariviriski <mafu.is.here@gmail.com>
2024-02-08 20:48:47 -0400

    Syntax

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -76,7 +76,7 @@ class Rectangle:
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
 
-    @@staticmethod
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """Returns the biggest rectangle based on area"""
         if not isinstance(rect_1, Rectangle):
