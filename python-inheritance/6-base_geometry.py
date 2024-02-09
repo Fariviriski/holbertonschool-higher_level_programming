9306f9a1fc870a9abe07d7e910fd5117ab55739e
Fariviriski <mafu.is.here@gmail.com>
2024-02-09 15:26:48 -0400

    task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""defines a base geometry class BaseGeometry"""
+
+
+class BaseGeometry:
+    """BaseGeometry"""
+
+    def area(self):
+        """not implemented"""
+        raise Exception("area() is not implemented")
