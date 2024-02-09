dbde06fe448e426bbcab8449fc90d9f645d1e434
Fariviriski <mafu.is.here@gmail.com>
2024-02-09 14:44:52 -0400

    Task 2

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+defines class-check func
+"""
+
+
+def is_same_class(obj, a_class):
+    """checks if obj is instance of a given class"""
+    if type(obj) == a_class:
+        return True
+    return False
