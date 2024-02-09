382dfbe620eeb4fa552bc3b40f99aac44e8514a3
Fariviriski <mafu.is.here@gmail.com>
2024-02-09 15:16:21 -0400

    nonsens

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -3,13 +3,14 @@
 
 
 def inherits_from(obj, a_class):
-    """checks if object is inherited instance of class
+     """Checks if an object is an inherited instance of a class.
+
     Args:
-        obj (any): the object to checks
-        a_class (type): the class to match object to
+        obj (any): The object to check.
+        a_class (type): The class to match the type of obj to.
     Returns:
-        True if object is inherited instance
-        False otherwise
+        If obj is an inherited instance of a_class - True.
+        Otherwise - False.
     """
     if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
