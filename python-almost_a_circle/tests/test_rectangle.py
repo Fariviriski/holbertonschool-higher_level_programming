aef4d08906bd656085cb72fefd1a6c48a5765a1e
Fariviriski <mafu.is.here@gmail.com>
2024-02-15 17:12:07 -0400

    Syntax line 182 on Rectangle test

--- python-almost_a_circle/tests/test_rectangle.py
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -180,7 +180,7 @@ class RectangleTest(unittest.TestCase):
         with self.assertRaises(TypeError) as e:
             r.area(5)
         self.assertEqual(
-            "area() takes 1 positional argument \
+            "Rectangle.area() takes 1 positional argument \
 but 2 were given",
             str(e.exception),
         )
